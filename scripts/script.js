63a17eb0a0f87bf45d5ce7dc354e9e336992ca82
fUNNY-fnaf <itsafeat@gmail.com>
2021-02-23 01:26:08 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 function klat() {
-				if (document.getElementById("ml").value != "") {
-								var val = document.getElementById("ml").value;
-				
-								var sum1 = parseFloat(val, 10) * 0.133 * 3.5;
-								var sum2 = parseFloat(val, 10) * 0.467 * 3.5;
+	if (document.getElementById("ml").value != "") {
+		var val = document.getElementById("ml").value;
+		var sum1 = parseFloat(val, 10) * 0.133 * 3.5;
+		var sum2 = parseFloat(val, 10) * 0.467 * 3.5;
 								
-        var text = "";
-        text = "Det vil være mellem " + sum1 + " og " + sum2 + " kalorier!";
-        text = text.split(" ");
-        len = text.length;
-        result = [];
+        	var text = "";
+        	text = "Det vil være mellem " + sum1 + " og " + sum2 + " kalorier!";
+	        text = text.split(" ");
+        	len = text.length;
+	        result = [];
         
-        text[4] = '<u>' + sum1.toFixed(3) + '</u>';
-        text[6] = '<u>' + sum2.toFixed(3) + '</u>'
+        	text[4] = '<u>' + sum1.toFixed(3) + '</u>';
+	        text[6] = '<u>' + sum2.toFixed(3) + '</u>'
         
-        if (isNaN(val)) {
-        				text = "Hvordan vil du konvertere " + val + " til kalorier? dumbass.";
-        } else {
-        				text.forEach(Element => {
-        								text = text.toString().replace(',', ' ');
-        				});
-        }
-        document.getElementById("res").innerHTML = text;
-    }
+        	if (isNaN(val)) {
+        		text = "Hvordan vil du konvertere <i>" + val + "</i> til kalorier? dumbass.";
+        	} else {
+        		text.forEach(Element => {
+        			text = text.toString().replace(',', ' ');
+        		});
+        	}
+        	document.getElementById("res").innerHTML = text;
+	}
 }
